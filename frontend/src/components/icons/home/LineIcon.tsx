4beb27d5d2508e7a54e7841e9236ce1b88243f9e
hfzfjr <hafizfajar0101@gmail.com>
2026-09-08 14:27:24 +0700

    Add home icon components for landing page

---
+++ frontend/src/components/icons/home/LineIcon.tsx
@@ -0,0 +1,7 @@
+export default function LineIcon() {
+    return (
+        <svg width="294" height="17" viewBox="0 0 294 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.5 6.63117C44.5 6.6311 153 -9.36875 292 15.1312" stroke="#004BDC" stroke-width="3" stroke-linecap="round" />
+        </svg>
+    )
+}
