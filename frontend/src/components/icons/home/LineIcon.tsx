386e083570c9623cb0aad62c18d002af9f6c3d0f
hfzfjr <hafizfajar0101@gmail.com>
2026-09-22 13:13:56 +0700

    Update LineIcon component

--- frontend/src/components/icons/home/LineIcon.tsx
+++ frontend/src/components/icons/home/LineIcon.tsx
@@ -1,7 +1,7 @@
-export default function LineIcon() {
+export default function LineIcon({ width = 294, strokeWidth = 3, className }: { width?: number; strokeWidth?: number; className?: string }) {
     return (
-        <svg width="294" height="17" viewBox="0 0 294 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 6.63117C44.5 6.6311 153 -9.36875 292 15.1312" stroke="#004BDC" strokeWidth="3" strokeLinecap="round" />
+        <svg width={width} height="17" viewBox="0 0 294 17" fill="none" className={className} xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.5 6.63117C44.5 6.6311 153 -9.36875 292 15.1312" stroke="#004BDC" strokeWidth={strokeWidth} strokeLinecap="round" />
         </svg>
     )
 }
